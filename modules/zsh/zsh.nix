3db15ad0e7cad7b1f5aac1ea1bb558188f5cf9de
Vinícius Daniel <dannful@gmail.com>
2024-12-02 10:42:25 -0300

    Improved zsh module

--- modules/zsh/zsh.nix
+++ modules/zsh/zsh.nix
@@ -1,28 +1,17 @@
-{ pkgs, lib, ... }:
+{ pkgs, lib, config, ... }:
 
 {
   home.packages = [ pkgs.any-nix-shell ];
   programs.zsh = {
     enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+    history = {
+      size = 10000;
+      path = "${config.xdg.dataHome}/zsh/history";
+    };
     plugins = [
-        {
-          name = "zsh-autosuggestions";
-          src = pkgs.fetchFromGitHub {
-            owner = "zsh-users";
-            repo = "zsh-autosuggestions";
-            rev = "v0.6.4";
-            sha256 = "0h52p2waggzfshvy1wvhj4hf06fmzd44bv6j18k3l9rcx6aixzn6";
-          };
-        }
-        {
-          name = "fast-syntax-highlighting";
-          src = pkgs.fetchFromGitHub {
-            owner = "zdharma";
-            repo = "fast-syntax-highlighting";
-            rev = "v1.55";
-            sha256 = "0h7f27gz586xxw7cc0wyiv3bx0x3qih2wwh05ad85bh2h834ar8d";
-          };
-        }
         {
           name = "powerlevel10k";
           src = pkgs.zsh-powerlevel10k;
