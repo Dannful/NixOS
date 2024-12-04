25a873c3d1f62a50078a5b65016e28f8ea15f19b
Vinícius Daniel <dannful@gmail.com>
2024-12-04 18:11:04 -0300

    Included shortcut for killing current window

--- modules/hyprland/custom-hyprland.nix
+++ modules/hyprland/custom-hyprland.nix
@@ -144,6 +144,7 @@ in {
           "$mod, Return, exec, ${pkgs.kitty}/bin/kitty"
           "$mod, D, exec, ${pkgs.rofi-wayland}/bin/rofi -show drun"
           "$mod, Q, exec, hyprctl kill"
+          "$mod, C, killactive"
           "$mod, F, fullscreen"
           '', Print, exec, grim -g "$(slurp)" - | swappy -f -''
         ] ++ (builtins.concatLists (builtins.genList (i:
