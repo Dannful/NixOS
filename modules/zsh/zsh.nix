d87762b2f68d76075f7f56b392daf254088311e1
Vinícius Daniel <dannful@gmail.com>
2024-12-05 09:09:03 -0300

    Added shell alias for kitten SSH

--- modules/zsh/zsh.nix
+++ modules/zsh/zsh.nix
@@ -7,6 +7,9 @@
     enableCompletion = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
+    shellGlobalAliases = {
+      kittyssh = "kitten ssh";
+    };
     history = {
       size = 10000;
       path = "${config.xdg.dataHome}/zsh/history";
