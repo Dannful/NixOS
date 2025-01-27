3615d664d5106f8e66f4a257ee9f4e729a5d72e9
Vinícius Daniel <dannful@gmail.com>
2025-01-27 18:15:13 -0300

    Put media info script to use follow metadata command

--- modules/eww/modules/scripts/media_info.sh
+++ modules/eww/modules/scripts/media_info.sh
@@ -1,29 +1,12 @@
 #!{BASH_BIN}
 
 IMAGE_DIR="$HOME/.config/eww/modules/music/images"
-LAST_SONG=""
-LAST_IMAGE=""
 EWW="{EWW_BIN} -c $HOME/.config/eww/modules/music"
 SCRIPT_NAME="media_info"
 LOG_SCRIPT="$HOME/.config/eww/modules/scripts/log.sh"
 
-select_player() {
-  playing_player=""
-  cover_player=""
-  total_player=""
-  playerctl -l 2> /dev/null | while read -r player;
-  do
-    art=`playerctl --player="$player" metadata mpris:artUrl 2> /dev/null`
-    status=`playerctl --player="$player" status 2> /dev/null`
-    [[ "$status" == "Playing" ]] && playing_player="$player"
-    [[ ! -z $art ]] && cover_player="$player"
-    [[ "$playing_player" == "$cover_player" ]] && total_player="$player"
-    [[ ! -z $cover_player ]] && player="$cover_player"
-    [[ ! -z $playing_player ]] && player="$playing_player"
-    [[ ! -z $total_player ]] && player="$total_player"
-    echo "$player"
-  done
-}
+LAST_POSITION=0
+LAST_LENGTH=0
 
 update_cover() {
   if [[ `echo $image | grep -c "file://"` -gt 0 ]]; then
@@ -42,40 +25,43 @@ update_cover() {
   $EWW update media_cover="$IMAGE_DIR/current.png"
 }
 
+playerctl --follow metadata mpris:artUrl | while IFS= read -r image; do
+  update_cover
+done &
+
+playerctl --follow metadata xesam:artist | while IFS= read -r artist; do
+  $EWW update media_artist="$artist"
+done &
+
+playerctl --follow metadata xesam:title | while IFS= read -r title; do
+  $EWW update media_title="$title"
+done &
+
 while true; do
   if [[ ! -z `playerctl status 2> /dev/null` ]]; then
-    player=`select_player 2> /dev/null | tail -1`
-    status=`playerctl --player="$player" status 2> /dev/null`
+    status=`playerctl status 2> /dev/null`
 
     if [[ "$status" == "Playing" ]]; then
       $EWW update media_status="󰏥"
 
       position=`playerctl --player="$player" position | awk '{ printf("%f", $1 * 1000000) }'`
       length=`playerctl --player="$player" metadata mpris:length`
+      if (( $position > $LAST_POSITION )); then
+        position=$(( $position - $LAST_POSITION ))
+        length=$(( $length - $LAST_LENGTH ))
+      fi
+      LAST_POSITION=$position
+      LAST_LENGTH=$length
       progress=`echo -n "$position $length" | awk '{ printf("%.2f", $1 / $2 * 100); }'`
       $EWW update media_progress=$progress
     else
       $EWW update media_status="󰐌"
     fi
-
-    title=`playerctl --player="$player" metadata xesam:title 2> /dev/null`
-
-    if [[ ! -z "$title" ]] && [[ "$title" != "$LAST_SONG" ]]; then
-      artist=`playerctl --player="$player" metadata xesam:artist 2> /dev/null`
-      $EWW update media_title="$title"
-      $EWW update media_artist="$artist"
-    fi
-    LAST_SONG="$title"
-
-    image=`playerctl metadata mpris:artUrl 2> /dev/null`
-    if [[ "$image" != "$LAST_IMAGE" ]]; then
-      update_cover &
-    fi
-    LAST_IMAGE="$image"
   else
     $EWW update media_cover="$IMAGE_DIR/default.png"
     $EWW update media_title="No title"
     $EWW update media_artist="No artist"
+    $EWW update media_progress=0
   fi
   sleep 0.3
 done
