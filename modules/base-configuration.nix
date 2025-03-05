54fe776b2f7a39621674c5a0e589bc16d6a27758
Vinícius Daniel <dannful@gmail.com>
2025-03-05 08:47:25 -0300

    Fixed an anime game launcher hash

--- modules/base-configuration.nix
+++ modules/base-configuration.nix
@@ -5,7 +5,7 @@ let
   cfg = config.base-config;
   aagl-gtk-on-nix = import (builtins.fetchTarball {
     url = "https://github.com/ezKEa/aagl-gtk-on-nix/archive/main.tar.gz";
-    sha256 = "01iwn0s8hbkgf9ip0wfnhs3dcdmgg55f0qj7b32rzqgajrfc1bag";
+    sha256 = "0f59radafvzdfn3ar1y6glx9ixc9hbvysaalsp492ixp8ihpkbxv";
   });
 in {
   imports = [ aagl-gtk-on-nix.module ];
