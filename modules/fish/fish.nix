e9a06fc6ebb05e736a9586828d2989ad6146a276
Vinícius Daniel <dannful@gmail.com>
2025-04-10 19:31:57 -0300

    Fixed Fish shell Tide not being properly configured

--- modules/fish/fish.nix
+++ modules/fish/fish.nix
@@ -10,11 +10,12 @@
     }];
 
     interactiveShellInit = ''
-      set fish_greeting
+      set -g fish_greeting
       eval "$(direnv hook fish)"
+
+      if not set -q tide_git_icon
+        tide configure --auto --style=Rainbow --prompt_colors="16 colors" --show_time="24-hour format" --rainbow_prompt_separators=Angled --powerline_prompt_heads=Round --powerline_prompt_tails=Round --powerline_prompt_style="Two lines, character and frame" --prompt_connection=Dotted --powerline_right_prompt_frame=Yes --prompt_spacing=Sparse --icons="Many icons" --transient=Yes
+      end
     '';
   };
-  home.activation.configure-tide = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    ${pkgs.fish}/bin/fish -c "tide configure --auto --style=Rainbow --prompt_colors='16 colors' --show_time='24-hour format' --rainbow_prompt_separators=Angled --powerline_prompt_heads=Round --powerline_prompt_tails=Round --powerline_prompt_style='Two lines, character and frame' --prompt_connection=Dotted --powerline_right_prompt_frame=Yes --prompt_spacing=Sparse --icons='Many icons' --transient=Yes"
-  '';
 }
