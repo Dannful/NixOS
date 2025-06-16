8926103b4c9e12f68a1e82078c12bc961d13bdc4
Vinícius Daniel <dannful@gmail.com>
2025-06-16 10:52:53 -0300

    Included C# nix flake template

---
+++ modules/flake-templates/csharp.nix
@@ -0,0 +1,26 @@
+{
+  inputs = { utils.url = "github:numtide/flake-utils"; };
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            permittedInsecurePackages =
+              [ "dotnet-sdk-6.0.428" "aspnetcore-runtime-6.0.36" ];
+          };
+        };
+        dotnetPackages = pkgs.dotnetCorePackages.combinePackages [
+          pkgs.dotnetCorePackages.sdk_6_0
+          pkgs.dotnetCorePackages.sdk_8_0
+          pkgs.dotnetCorePackages.aspnetcore_6_0
+        ];
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = [ dotnetPackages pkgs.omnisharp-roslyn ];
+          shellHook = ''
+            export DOTNET_ROOT="${dotnetPackages}";
+          '';
+        };
+      });
+}
