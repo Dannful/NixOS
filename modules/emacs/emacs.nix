511c621b35c3d8c3306a3bed482d5a7d47eaf43c
Vinícius Daniel <dannful@gmail.com>
2025-09-24 15:36:07 -0300

    Included vterm in Emacs

--- modules/emacs/emacs.nix
+++ modules/emacs/emacs.nix
@@ -1,16 +1,14 @@
-# emacs-config.nix
-{
-  pkgs, ...
-}:
+{ pkgs, ... }:
 
 {
   programs.emacs = {
     enable = true;
     package = pkgs.emacs-pgtk;
-    extraPackages = epkgs: [ epkgs.evil ];
+    extraPackages = epkgs: [ epkgs.evil epkgs.vterm ];
     extraConfig = ''
       (require 'evil)
       (evil-mode 1)
+      (require 'vterm)
     '';
   };
 }
