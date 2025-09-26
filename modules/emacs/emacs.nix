8d39b8c7497b015737496076bc6fcf00a230c9f0
Vinícius Daniel <dannful@gmail.com>
2025-09-26 18:26:53 -0300

    Included Babel R for Emacs

--- modules/emacs/emacs.nix
+++ modules/emacs/emacs.nix
@@ -4,12 +4,20 @@
   programs.emacs = {
     enable = true;
     package = pkgs.emacs-pgtk;
-    extraPackages = epkgs: [ epkgs.evil epkgs.vterm epkgs.doom-themes ];
+    extraPackages = epkgs: [
+      epkgs.evil
+      epkgs.vterm
+      epkgs.doom-themes
+      epkgs.ess
+    ];
     extraConfig = ''
       (require 'evil)
       (evil-mode 1)
       (require 'vterm)
       (load-theme 'doom-one t)
+      (org-babel-do-load-languages
+       'org-babel-load-languages
+       '((R . t)))
     '';
   };
 }
