7310e38961343f67ffd1065363acd08f48e5121c
Vinícius Daniel <dannful@gmail.com>
2025-10-29 19:43:40 -0300

    Customized Neorg

--- modules/nvim/nvim.nix
+++ modules/nvim/nvim.nix
@@ -146,6 +146,35 @@
             "RainbowCyan"
           ];
         };
+        notes.neorg = {
+          enable = true;
+          setupOpts.load = {
+            "core.defaults" = {};
+            "core.completion" = {
+              config = {
+                engine = "nvim-cmp";
+                name = "[Norg]";
+              };
+            };
+            "core.integrations.nvim-cmp" = {};
+            "core.concealer" = {config = {icon_preset = "diamond";};};
+            "core.esupports.metagen" = {
+              config = {
+                type = "auto";
+                update_date = true;
+              };
+            };
+            "core.qol.toc" = {};
+            "core.qol.todo_items" = {};
+            "core.looking-glass" = {};
+            "core.presenter" = {config = {zen_mode = "zen-mode";};};
+            "core.export" = {};
+            "core.export.markdown" = {config = {extensions = "all";};};
+            "core.summary" = {};
+            "core.tangle" = {config = {report_on_empty = false;};};
+            "core.ui.calendar" = {};
+          };
+        };
       };
     };
   };
