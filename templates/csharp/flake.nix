3f71ee6b63cde20bf758978b68d31b6c0133be70
Vinícius Daniel <dannful@gmail.com>
2026-01-07 12:01:30 -0300

    Using .NET 9 in template

--- templates/csharp/flake.nix
+++ templates/csharp/flake.nix
@@ -1,26 +1,28 @@
 {
-  inputs = { utils.url = "github:numtide/flake-utils"; };
-  outputs = { self, nixpkgs, utils }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          config = {
-            permittedInsecurePackages =
-              [ "dotnet-sdk-6.0.428" "aspnetcore-runtime-6.0.36" ];
-          };
+  inputs = {utils.url = "github:numtide/flake-utils";};
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+  }:
+    utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        config = {
+          permittedInsecurePackages = ["dotnet-sdk-6.0.428" "aspnetcore-runtime-6.0.36"];
         };
-        dotnetPackages = pkgs.dotnetCorePackages.combinePackages [
-          pkgs.dotnetCorePackages.sdk_6_0
-          pkgs.dotnetCorePackages.sdk_8_0
-          pkgs.dotnetCorePackages.aspnetcore_6_0
-        ];
-      in {
-        devShell = pkgs.mkShell {
-          buildInputs = [ dotnetPackages pkgs.omnisharp-roslyn ];
-          shellHook = ''
-            export DOTNET_ROOT="${dotnetPackages}";
-          '';
-        };
-      });
+      };
+      dotnetPackages = pkgs.dotnetCorePackages.combinePackages [
+        pkgs.dotnetCorePackages.sdk_6_0
+        pkgs.dotnetCorePackages.sdk_9_0
+        pkgs.dotnetCorePackages.aspnetcore_6_0
+      ];
+    in {
+      devShell = pkgs.mkShell {
+        buildInputs = [dotnetPackages];
+        shellHook = ''
+          export DOTNET_ROOT="${dotnetPackages}";
+        '';
+      };
+    });
 }
